769e09d025a5b83dafcc386bbb281cfc40eeb070
Tun72 <tunt72553@gmail.com>
2024-05-11 23:18:31 +0630

    master

--- src/features/Recipes/RecipeList.jsx
+++ src/features/Recipes/RecipeList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiStar } from "react-icons/hi2";
 export default function RecipeList({ name, image, id }) {
   return (
-    <li className="flex w-[250px] flex-col">
+    <li className="group flex w-[250px] cursor-pointer flex-col">
       <div className="h-[13rem] w-[100%] overflow-hidden rounded-[2rem]">
         <img
           src={image}
@@ -10,8 +10,8 @@ export default function RecipeList({ name, image, id }) {
           className="block h-[100%] w-[100%] object-cover"
         />
       </div>
-      <div className="p-[10px_15px]">
-        <p className="line-clamp-1 text-3xl">{name}</p>
+      <div className="rounded-[1.5rem]  p-[10px_15px] transition-all duration-300 group-hover:translate-x-[-1.4rem]  group-hover:mt-[-2.5rem]  group-hover:bg-[#0de39d]  group-hover:p-[1.5rem_2rem]">
+        <p className="line-clamp-1 text-2xl">{name}</p>
         <p className="flex items-center gap-[0.5rem]">
           <HiStar className="text-[1.5rem] font-bold opacity-[0.6]" />
           <span>
